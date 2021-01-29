8ab6ec216c6e84bdf15cdc159e1c1f5db73f106c
Daniel Guerra <daniel.guerra@smartitsystems.com.br>
2021-01-29 19:29:28 -0300

    Alteração de projeto para versão 2.1.7.RELEASE do Spring.

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -1,12 +1,11 @@
 package com.algaworks.algafood;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlgafoodApiApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
